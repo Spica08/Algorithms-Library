3be91871db077705739cadc9b8168f5345dd7115
Spica08 <spica_maron_1134@icloud.com>
2024-04-21 22:38:16 +0900

    tweak BigInteger

--- Math/BigIntegers.cpp
+++ Math/BigIntegers.cpp
@@ -1,14 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using ull = unsigned long long;
-using Matrix = vector<vector<ll>>;
-const int inf = 1000000000;
-const ll INF = 1000000000000000000;
-const ll mod = 998244353;
-const ull mod_hash = (1UL << 61) - 1;
-const vector<int> dx = {0, 1, 0, -1, 1, 1, -1, -1};
-const vector<int> dy = {1, 0, -1, 0, 1, -1, 1, -1};
 
 struct BigInteger{
   vector<int> value;
@@ -173,11 +164,6 @@ void Verify(){
 }
 
 int main(){
-  //Verify(); //Addition
-  string x = "100";
-  string y = "48";
-  BigInteger X(x), Y(y);
-  (X+Y).print_val();
-  (X-Y).print_val();
+  Verify(); //Addition
   return 0;
 }
