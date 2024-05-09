5c7d5063536f5006e2b917564b34d16607d9d334
Spica08 <spica_maron_1134@icloud.com>
2024-05-09 21:25:17 +0900

    fixed Compress.cpp

--- Utility/Compress.cpp
+++ Utility/Compress.cpp
@@ -1,17 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using Graph = vector<vector<int>>;
-using Grid = vector<vector<char>>;
 
+// int or long long
 template <typename T>
-void compress(vector<T> &A) {
-  vector<T> vals = A;
-  sort(vals.begin(), vals.end());
-  vals.erase(unique(vals.begin(), vals.end()), vals.end());
-  for (int i = 0; i < A.size(); i++) {
-    A[i] = lower_bound(vals.begin(), vals.end(), A[i]) - vals.begin();
-  }
+struct Compress{
+  vector<T> A;
+  vector<T> reconstruct;
+
+  Compress(vector<T> C) : A(C), reconstruct(1000000){
+    vector<T> vals = A;
+    sort(vals.begin(), vals.end());
+    vals.erase(unique(vals.begin(), vals.end()), vals.end());
+    for (int i = 0; i < A.size(); i++) {
+      T b = A[i];
+      A[i] = lower_bound(vals.begin(), vals.end(), A[i]) - vals.begin();
+      reconstruct[A[i]] = b;
+    }
+  };
+
+  const T& operator[](int i)const{return reconstruct[i];};
+};
+
+void Verify(){
+  /*
+    Verify with ABC036-C (座圧)
+      https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_6_A
+   */
+
+  //Input
+  int N; cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) cin >> A[i];
+  
+  //Build
+  Compress<int> comp(A);
+  
+  //Solve
+  for(auto a : comp.A) cout << a << endl;
 }
 
-int main() { return 0; }
+int main() { 
+  Verify();
+  return 0; 
+}
