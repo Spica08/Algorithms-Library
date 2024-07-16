b1f3eb2feb0ad267d7abcc08e111bd811348a8a6
Spica08 <spica_maron_1134@icloud.com>
2024-07-16 16:53:18 +0900

    create Utility

---
+++ Utility/templete.hpp
@@ -0,0 +1,16 @@
+#ifndef TEMPLETE_HPP
+#define TEMPLETE_HPP
+
+#include "mystd.hpp"
+
+using ll = long long;
+using ull = unsigned long long;
+using Matrix = std::vector<std::vector<ll>>;
+const int inf = 1000000000;
+const ll INF = 1000000000000000000;
+const ll mod = 998244353;
+const ull mod_hash = (1UL << 61) - 1;
+const std::vector<int> dx = {0, 1, 0, -1, 1, 1, -1, -1};
+const std::vector<int> dy = {1, 0, -1, 0, 1, -1, 1, -1};
+
+#endif //TEMPLETE_HPP
