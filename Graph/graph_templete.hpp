aa3478154e866fc64f2bde17412fcd875006daba
Spica08 <spica_maron_1134@icloud.com>
2024-07-17 17:33:05 +0900

    create graph_templete

---
+++ Graph/graph_templete.hpp
@@ -0,0 +1,46 @@
+#ifndef GRAPH_TEMPLETE_HPP
+#define GRAPH_TEMPLETE_HPP
+
+#include "../Utility/templete.hpp"
+
+template<typename T>
+class WeightedGraph{
+  struct Edge{
+    int from, to, id;
+    T cost;
+    Edge(int from, int to, int id, T cost):from(from), to(to), id(id), cost(cost){}
+  };
+  public:
+    WeightedGraph(int N): N(N), G(N){}
+    void add_edge(int u, int v, int id, T cost, int zero_indexed = 0, int directed = 0){
+      if (!zero_indexed) {u--; v--;}
+      G[u].emplace_back(u, v, id, cost);
+      if(!directed)G[v].emplace_back(v, u, id, cost);
+    }
+    int size()const{return G.size();};
+    const std::vector<Edge>& operator[](int i)const{return G[i];};
+  private:
+    int N;
+    std::vector<std::vector<Edge>> G;
+};
+
+class UnweightedGraph{
+  public:
+    UnweightedGraph(int N): N(N), G(N){}
+    void read_edge(int zero_indexed = 0, int directed = 0){
+      int u, v; std::cin >> u >> v; 
+      add_edge(u, v, zero_indexed, directed);
+    }
+    void add_edge(int u, int v, int zero_indexed = 0, int directed = 0){
+      if (!zero_indexed) {u--; v--;}
+      G[u].push_back(v);
+      if(!directed)G[v].push_back(u);
+    }
+    int size()const{return G.size();};
+    const std::vector<int>& operator[](int i)const{return G[i];};
+  private:
+    int N;
+    std::vector<std::vector<int>> G;
+};
+
+#endif //GRAPH_TEMPLETE_HPP
---
+++ Verify/Graph/graph_templete.cpp
@@ -0,0 +1,58 @@
+#include "../../Graph/graph_templete.hpp"
+
+void Verify_1(){
+  /*
+    Verify with ABC276-B (Adjacency List)
+      https://atcoder.jp/contests/abc276/tasks/abc276_b
+   */
+
+  //Input
+  int N, M; std::cin >> N >> M;
+  UnweightedGraph G(N);
+  for (int i = 0; i < M; i++) {
+    G.read_edge(0, 0);
+  }
+
+  //Solve
+  for(int i = 0; i < N; i++){
+    std::vector<int> t = G[i];
+    sort(t.begin(), t.end());
+    std::cout << t.size() << " ";
+    for(int n : t){
+      std::cout << n + 1 << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+void Verify_2(){
+  /*
+    Verify with ABC276-B (Adjacency List)
+      https://atcoder.jp/contests/abc276/tasks/abc276_b
+   */
+
+  //Input
+  int N, M; std::cin >> N >> M;
+  WeightedGraph<ll> G(N);
+  for (int i = 0; i < M; i++) {
+    int a, b; std::cin >> a >> b;
+    G.add_edge(a, b, i, 1, 0, 0);
+  }
+
+  //Solve
+  for(int i = 0; i < N; i++){
+    std::vector<int> t;
+    for(auto n : G[i]) t.push_back(n.to);
+    sort(t.begin(), t.end());
+    std::cout << t.size() << " ";
+    for(int n : t){
+      std::cout << n + 1 << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(){
+  //Verify_1(); //UnweghtedGraph
+  Verify_2(); //WeghtedGraph
+}
